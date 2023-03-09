bef84f674b3c44f3d8b08ba85c172c65768acb87
Jan Hladík <hladik@silixcon.com>
2023-03-09 15:51:59 +0100

    add test script and correct output files

---
+++ test/test.cmd
@@ -0,0 +1,25 @@
+git status
+
+REM kiasm switch --prefix def ..\
+kiasm switch --prefix var2 ..\
+kikit fab jlcpcb --no-drc --assembly --schematic ..\asmTest.kicad_sch ..\asmTest.kicad_pcb grb
+
+git diff --no-index grb_var2\bom.csv grb\bom.csv
+rmdir /Q /s grb
+
+kiasm switch --prefix def ..\
+kikit fab jlcpcb --no-drc --assembly --schematic ..\asmTest.kicad_sch ..\asmTest.kicad_pcb grb
+git diff --no-index grb_def\bom.csv grb\bom.csv
+rmdir /Q /s grb
+
+REM kiasm switch --prefix var2 ..\
+kiasm switch --prefix var1 ..\
+kikit fab jlcpcb --no-drc --assembly --schematic ..\asmTest.kicad_sch ..\asmTest.kicad_pcb grb
+git diff --no-index grb_var1\bom.csv grb\bom.csv
+rmdir /Q /s grb
+
+git status
+
+
+REM git diff --no-index --color-words=. grb_var2\bom.csv grb\bom.csv
+REM git diff --no-index --color-words grb_var2\bom.csv grb\bom.csv
